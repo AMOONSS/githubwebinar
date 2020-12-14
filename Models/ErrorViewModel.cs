6c67879fed2634e0bc77adf9bbe58cf272dc8b28
AMOONSS <amoonsulthana29@gmail.com>
2020-12-14 11:39:57 +0530

    updated

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gitwebinar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
